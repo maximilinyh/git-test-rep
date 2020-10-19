5ee209c707e91827c46825c9bf1c3815789c42f0
Maxim Ilinyh <maxim_ilinyh@mail.ru>
2020-10-19 22:49:08 +0700

    add sum function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { fuchsia } = require("color-name");
+
 document.querySelectorAll('.test').addEventListener('click',()=>{
     alert('test');
 })
@@ -10,3 +12,9 @@ test();
 
 console.log('new');
 console.log('super new');
+
+function sum (a, b) {
+    return a + b;
+};
+
+sum(1, 2);
